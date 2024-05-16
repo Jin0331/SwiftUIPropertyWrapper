879e9160cbc93c5ecdf5554926d13aea889c05db
Jin0331 <sempre813@naver.com>
2024-05-17 00:36:49 +0900

    Initial Commit

---
+++ SwiftUIPropertyWrapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIPropertyWrapper
+//
+//  Created by JinwooLee on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIPropertyWrapper/SwiftUIPropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPropertyWrapperApp.swift
+//  SwiftUIPropertyWrapper
+//
+//  Created by JinwooLee on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
